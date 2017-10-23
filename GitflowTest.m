d2c509f8624c6a771d80abf7307c6831a0d4faaf
Josh Sanders <josh@sanworks.io>
2017-10-22 20:05:26 -0400

    -Added main file

---
+++ GitflowTest.m
@@ -0,0 +1,12 @@
+function Result = GitflowTest(TestNum)
+
+switch TestNum
+    case 1
+        disp('Test 1');
+    case 2
+        disp('Test 2');
+    case 3
+        disp('Test 3');
+    case 4
+        disp('Test 4');
+end
